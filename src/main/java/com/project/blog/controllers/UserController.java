19f94a4e20a0ab63c80c65f19f29cffa177635f7
DarshanVala01 <darshanvala024@gmail.com>
2025-03-25 15:04:24 +0530

    all user controller api

--- src/main/java/com/project/blog/controllers/UserController.java
+++ src/main/java/com/project/blog/controllers/UserController.java
@@ -3,11 +3,16 @@ package com.project.blog.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.blog.payloads.ApiResponse;
 import com.project.blog.payloads.UserDTO;
 import com.project.blog.services.UserService;
 
@@ -26,8 +31,23 @@ public class UserController {
 	}
 	
 	// Update user
+	@PutMapping("/update/{id}")
+	public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDto, @PathVariable("id") Integer user_id){
+		UserDTO updatedUser = this.userService.updateUser(userDto, user_id);
+		return ResponseEntity.ok(updatedUser);
+	}
 	
 	// Delete user
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("id") Integer user_id) {
+		this.userService.deleteUser(user_id);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully",true),HttpStatus.OK);
+	}
 	
 	// Get user
+	@GetMapping("/get/{id}")
+	public ResponseEntity<UserDTO> getUserById(@PathVariable("id") Integer user_id){
+		UserDTO userDto = this.userService.getUserById(user_id);
+		return new ResponseEntity<UserDTO>(userDto,HttpStatus.OK);
+	}
 }
